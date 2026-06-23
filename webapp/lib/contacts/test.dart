fbd9012cdf4e6231b282ead277c469225a58689a
Flutter Painter <dev@weebi.com>
2026-06-23 07:34:37 +0200

    chore: update .gitignore and modify dependency overrides
    - Updated dependency overrides in pubspec_overrides.yaml to reflect changes in managed dependencies.
    - Refactored test.dart to utilize a contact map for creating ContactPb instances.

--- webapp/lib/contacts/test.dart
+++ webapp/lib/contacts/test.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:models_weebi/models.dart';
 import 'package:protos_weebi/protos_weebi_io.dart';
 import 'package:web_admin/views/mirror.dart';
 
 void main() {
+  final contact = {
+    'id': 1,
+    'firstName': 'John',
+    'lastName': 'Doe',
+    'mail': 'john@doe.com',
+  };
   // Example usage
   var myMessages = [
-    ContactPb.create()
-      ..mergeFromProto3Json(ContactWeebi.dummy.toMap(),
-          ignoreUnknownFields: true),
-    ContactPb.create()
-      ..mergeFromProto3Json(ContactWeebi.dummy2.toMap(),
-          ignoreUnknownFields: true),
+    ContactPb.create()..mergeFromProto3Json(contact, ignoreUnknownFields: true),
+    ContactPb.create()..mergeFromProto3Json(contact, ignoreUnknownFields: true),
   ];
   runApp(MaterialApp(
     home: Scaffold(
